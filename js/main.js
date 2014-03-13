b1a96c1006820eedba7d33ef1711e056033fbe17
Keanna Sion <sion.keanna@gmail.com>
2014-03-13 03:32:22 -0400

    Deleted old stuffs switched to foundation...pulling feeds using google api!!

--- js/main.js
+++ js/main.js
@@ -1,16 +1,47 @@
-$(document).ready(function() {
-  $.ajax({
-    beforeSend: function(xhr) {
-      xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-    },
-    crossDomain: true,
-    url: "http://rss.allrecipes.com/daily.aspx?hubID=80",
-    dataType: "jsonp",
-    success: function() {
-      alert("success!");
-    },
-    error: function() {
-    	alert("fail!"); 
-    }
-  });
+﻿var arrayPanels = [];
+$(document).ready(function () {
+
+    var feed1 = new google.feeds.Feed("http://rss.allrecipes.com/daily.aspx?hubID=80");
+    var feed2 = new google.feeds.Feed("http://www.kraftfoods.com/rss/dinnerRecipes.aspx");
+    var feed3 = new google.feeds.Feed("http://www.food.com/rss");
+
+    feed1.setNumEntries(10);
+    feed2.setNumEntries(10);
+    feed3.setNumEntries(10);
+
+    feed1.load(function (results) {
+        if (!results.error) {
+            var entries = results.feed.entries;
+            display_array_panels(entries);
+        }
+    });
+
+    feed2.load(function (results) {
+        if (!results.error) {
+            var entries = results.feed.entries;
+            display_array_panels(entries);
+        }
+    });
+
+    feed3.load(function (results) {
+        if (!results.error) {
+            var entries = results.feed.entries;
+            display_array_panels(entries);
+        }
+    });
 });
+
+function display_array_panels(entries) {
+    entries.forEach(function (entry) {
+        var entryTemplate = $(".entry", $(".templates")).clone();
+        var title = entryTemplate.find(".entryTitle");
+        var pubDate = entryTemplate.find(".entryDate");
+        var content = entryTemplate.find(".entryContent");
+
+        title.text(entry.title);
+        pubDate.text(entry.publishedDate);
+        content.text(entry.contentSnippet);
+
+        $(".feedsPanel").append(entryTemplate);
+    });
+}
